36f481fcda8550457fee8fc157cec5362331d2d0
xiaohuoni <448627663@qq.com>
2021-09-14 18:35:02 +0800

    chore: code style

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,13 +1,15 @@
-import { InfiniteScroll, List } from 'antd-mobile'
-import React, { useState } from 'react'
+import { InfiniteScroll, List } from 'antd-mobile';
+import React, { useState } from 'react';
+import { history } from 'umi';
 
-let count = 0
+let count = 0;
 
 async function mockRequest() {
+  console.log('mockRequest');
   if (count >= 5) {
-    return []
+    return [];
   }
-  count++
+  count++;
   return [
     'A',
     'B',
@@ -26,23 +28,23 @@ async function mockRequest() {
     'O',
     'P',
     'Q',
-  ]
+  ];
 }
 
 export default () => {
-  const [data, setData] = useState<string[]>([])
-  const [hasMore, setHasMore] = useState(true)
+  const [data, setData] = useState<string[]>([]);
+  const [hasMore, setHasMore] = useState(true);
   async function loadMore() {
-    const append = await mockRequest()
-    setData(val => [...val, ...append])
-    setHasMore(append.length > 0)
+    const append = await mockRequest();
+    setData(val => [...val, ...append]);
+    setHasMore(append.length > 0);
   }
 
   return (
     <>
       <List>
         {data.map((item, index) => (
-          <List.Item key={index}>{item}</List.Item>
+          <List.Item key={index} onClick={() => history.goBack()}>{item}</List.Item>
         ))}
       </List>
       <InfiniteScroll loadMore={loadMore} hasMore={hasMore} />
