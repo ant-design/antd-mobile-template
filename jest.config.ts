6d6e36005110fa8687b8ea94cebf5b3639520d8c
xiaohuoni <chen.xiaocong2@iwhalecloud.com>
2022-07-08 11:12:14 +0800

    fix: test

--- jest.config.ts
+++ jest.config.ts
@@ -8,5 +8,8 @@ export default async () => {
     setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
     // if you require some es-module npm package, please uncomment below line and insert your package name
     // transformIgnorePatterns: ['node_modules/(?!.*(lodash-es|your-es-pkg-name)/)']
+    transformIgnorePatterns: [
+      'node_modules/(?!.*(@umijs/renderer-react|umi/client/client/|@umijs/plugins/libs|antd-mobile/es/))',
+    ],
   })) as Config.InitialOptions;
 };
